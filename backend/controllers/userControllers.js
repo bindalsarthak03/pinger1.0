56eb78840b88d2796c8f8c1a44337a5313cdce4a
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2023-05-08 16:57:32 +0530

    password encryption with bcrypt

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,7 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
 const generateToken = require("../config/generateToken");
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password, pic } = req.body;
@@ -28,13 +28,28 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      password:user.password,
       pic: user.pic,
-      token: generateToken(user._id)
+      token: generateToken(user._id),
     });
-  }else{
-    res.status(400)
-    throw new Error ("Failed to create new user")
+  } else {
+    res.status(400);
+    throw new Error("Failed to create new user");
   }
 });
 
-module.exports = registerUser
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      pic: user.pic,
+      token: generateToken(user._id),
+    });
+  }
+});
+
+module.exports = {registerUser,authUser};
