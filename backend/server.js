02717b9c754c8dd1221821008991d9eb176b1db2
Sarthak Bindal <78961306+bindalsarthak03@users.noreply.github.com>
2023-06-30 18:16:59 +0530

    create chat api

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const { chats } = require("./data/data");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const app = express();
 dotenv.config();
@@ -13,6 +14,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/chat",chatRoutes);
 app.use(notFound);
 app.use(errorHandler)
 
